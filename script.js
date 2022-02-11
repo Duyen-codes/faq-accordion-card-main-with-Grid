79c96b8ead53b194138875fe60848f8e6c38e423
Duyen Nguyen <hongduyen0705@gmail.com>
2022-02-11 11:42:56 +0200

    init commit solution with grid

---
+++ script.js
@@ -0,0 +1,18 @@
+const items = [...document.querySelectorAll('li.faq__faq-item')];
+
+items.forEach(item => {
+    item.addEventListener('click', e => {
+        const clickedItem = e.target.closest('li')
+        const itemDetail = clickedItem.querySelector('.faq-item__detail')
+        const clickedItemActive = clickedItem.classList.contains('active')
+        clickedItem.classList.toggle('active')
+        if (clickedItemActive) {
+            itemDetail.style.maxHeight = null;
+        } else {
+            const scrollHeight = itemDetail.scrollHeight;
+            itemDetail.style.maxHeight = `${scrollHeight}px`;
+        }
+    })
+})
+
+items[1].click();
